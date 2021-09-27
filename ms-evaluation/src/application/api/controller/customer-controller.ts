d21c3929690c84d43a35d5726341174077a2c50f
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-27 19:54:49 -0300

    feat: add CustomerController

--- ms-evaluation/src/application/api/controller/customer-controller.ts
+++ ms-evaluation/src/application/api/controller/customer-controller.ts
@@ -1,12 +1,38 @@
 import { Request, Response } from "express";
 import { CustomerService } from "../../services/customer-service";
 
+const customerService = new CustomerService()
+
 export default class CustomerController {
-  constructor(private customerService: CustomerService = new CustomerService()) {}
 
   public async index(request: Request, response: Response) {
-    const customers = await this.customerService.index()
+
+    let { page, limit } = request.query
+    let pagination = Number(page)
+    let limitation = Number(limit)
+
+    pagination = !page || pagination <= 0 || Number.isNaN(pagination) ? 1 : pagination
+    limitation = !limitation || limitation <= 0 || Number.isNaN(pagination) ? 10 : limitation
+    const customers = await customerService.index(pagination, limitation)
 
     return response.status(200).send({ customers: customers })
+  }
+
+  public async show(request: Request, response: Response) {
+    const id = request.params.id
+
+    try {
+      const customer = await customerService.show(Number(id))
+      return response.send({ customer: customer })
+    } catch(err) {
+      return response.status(404).send({ errors: [ { message: err.message } ] })
+    }
+  }
+
+  public async store(request: Request, response: Response) {
+    const data = request.body
+    const customer = await customerService.store({ ...data })
+
+    return response.status(201).send({ customer: customer })
   }
 }
