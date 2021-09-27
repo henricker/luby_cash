faebdffa633525439989573408e40e6308f919ec
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-27 19:55:44 -0300

    feat: add customer service

--- ms-evaluation/src/application/services/customer-service.ts
+++ ms-evaluation/src/application/services/customer-service.ts
@@ -1,4 +1,4 @@
-import { getCustomRepository } from "typeorm";
+import Customer from "../entity/customer";
 import { CustomerRepository } from "../repository/customer-repository";
 
 interface ICreateCustomer {
@@ -9,20 +9,30 @@ interface ICreateCustomer {
 
 export class CustomerService {
 
-  constructor(private customerRepository: CustomerRepository = getCustomRepository(CustomerRepository)) {}
+  constructor(private customerRepository: CustomerRepository = new CustomerRepository()) {}
 
-  public async store({ name, email, averageSalary }: ICreateCustomer): Promise<void> {
+  public async store({ name, email, averageSalary }: ICreateCustomer): Promise<Customer> {
     const status = this.evaluation(averageSalary)
-    const customer = this.customerRepository.create({ name, email, averageSalary, status })
-    await this.customerRepository.save(customer)
+    const customer = this.customerRepository.create({ name, email, average_salary: averageSalary, status, created_at: new Date() })
+    return customer
   }
 
-  public async index(take: number = 10, skip: number = 1) {
-    const customers = await this.customerRepository.find({ take, skip })
+  public async index(page: number = 0, limit: number = 10) {
+    const customers = await this.customerRepository.find({ page, limit })
     return customers
+  }
+ 
+  public async show(id: number) {
+    const customer = await this.customerRepository.findById(id)
+    
+    if(!customer)
+      throw new Error('customer not found')
+
+    return customer
   }
 
   private evaluation(averageSalary: number): boolean {
     return averageSalary < 500 ? false : true
   }
+
 }
