c8d5a08fc79f8519f052ede81182781b25bca2d9
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-28 14:13:45 -0300

    test: index test implementeds

--- ms-evaluation/__tests__/unit/customer-controller.spec.ts
+++ ms-evaluation/__tests__/unit/customer-controller.spec.ts
@@ -88,9 +88,85 @@ describe('CustomerController', () => {
         })
       })
     })
-    describe('#Index', () => {
-      test.todo('should call index method of service, find method of repository, and query method of client postgres, when store method of controller is called')
-      test.todo('should call send method of response with array of customers')
+  describe('#Index', () => {
+    afterEach(() => {
+      jest.restoreAllMocks()
+    })
+    test('should call index method of service, find method of repository, and query method of client postgres, when store method of controller is called', async () => {
+      jest.spyOn(Client.prototype, 'connect').mockImplementation()
+      jest.spyOn(Client.prototype, 'end').mockImplementation()
+
+      const customerMockedClientPg: QueryResult<any> = {
+        rows: [
+          { name: 'Joe Doe', email: 'Doe@email.com', average_salary: 6000, status: true, id: 1 },
+          { name: 'Doe Joe', email: 'Joe@email.com', average_salary: 500, status: true, id: 2 },
+          { name: 'Timber Mcarty', email: 'timber@email.com', average_salary: 300, status: false, id: 3 },
+          { name: 'Lucian', email: 'lucian@email.com', average_salary: 200, status: false, id: 4 },
+          { name: 'Dr mundo', email: 'dr@email.com', average_salary: 8000, status: true, id: 6 }
+        ],
+        command: '',
+        rowCount: 1,
+        oid: 0,
+        fields: []
+      }
+
+      jest.spyOn(Client.prototype, 'query').mockImplementation((text, values) => {
+        return customerMockedClientPg
+      })
+
+      jest.spyOn(CustomerRepository.prototype, 'find')
+      jest.spyOn(CustomerService.prototype, 'index')
+
+      response['send'] = jest.fn()
+
+      request.query = { page: '0', limit: '10' }
+
+      await CustomerController.prototype.index(request, response)
+      expect(Client.prototype.query).toBeCalled()
+      expect(CustomerRepository.prototype.find).toBeCalled()
+      expect(CustomerService.prototype.index).toBeCalled()
+    })
+    test('should call send method of response with array of customers', async () => {
+        jest.spyOn(Client.prototype, 'connect').mockImplementation()
+        jest.spyOn(Client.prototype, 'end').mockImplementation()
+
+        const dateCreated = new Date()
+        const customerMockedClientPg: QueryResult<any> = {
+          rows: [
+            { name: 'Joe Doe', email: 'Doe@email.com', average_salary: 6000, status: true, id: 1, created_at: dateCreated },
+            { name: 'Doe Joe', email: 'Joe@email.com', average_salary: 500, status: true, id: 2, created_at: dateCreated },
+            { name: 'Timber Mcarty', email: 'timber@email.com', average_salary: 300, status: false, id: 3, created_at: dateCreated },
+            { name: 'Lucian', email: 'lucian@email.com', average_salary: 200, status: false, id: 4, created_at: dateCreated },
+            { name: 'Dr mundo', email: 'dr@email.com', average_salary: 8000, status: true, id: 6, created_at: dateCreated }
+          ],
+          command: '',
+          rowCount: 1,
+          oid: 0,
+          fields: []
+        }
+
+        jest.spyOn(Client.prototype, 'query').mockImplementation((text, values) => {
+          return customerMockedClientPg
+        })
+
+        jest.spyOn(CustomerRepository.prototype, 'find')
+        jest.spyOn(CustomerService.prototype, 'index')
+
+        response['send'] = jest.fn()
+
+        request.query = { page: '0', limit: '10' }
+
+        await CustomerController.prototype.index(request, response)
+        expect(response.send).toBeCalledWith({ 
+          customers: [
+            new Customer({ name: 'Joe Doe', email: 'Doe@email.com', average_salary: 6000, status: true, id: 1, createdAt: dateCreated }),
+            new Customer({ name: 'Doe Joe', email: 'Joe@email.com', average_salary: 500, status: true, id: 2, createdAt: dateCreated }),
+            new Customer({ name: 'Timber Mcarty', email: 'timber@email.com', average_salary: 300, status: false, id: 3, createdAt: dateCreated }),
+            new Customer({ name: 'Lucian', email: 'lucian@email.com', average_salary: 200, status: false, id: 4, createdAt: dateCreated }),
+            new Customer({ name: 'Dr mundo', email: 'dr@email.com', average_salary: 8000, status: true, id: 6, createdAt: dateCreated })
+          ] 
+        })
+      })
     })
   })
 })
