05f8b8d435bbb9ea2db53002359e16be7453d961
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-28 10:47:32 -0300

    test: add unit test of store method of controller

---
+++ ms-evaluation/__tests__/unit/customer-controller.spec.ts
@@ -0,0 +1,49 @@
+import { request, response } from 'express'
+import { Client, QueryResult } from 'pg'
+import CustomerController from '../../src/application/api/controller/customer-controller'
+import { CustomerRepository } from '../../src/application/repository/customer-repository'
+import { CustomerService } from '../../src/application/services/customer-service'
+
+describe('CustomerController', () => {
+  describe('#store', () => {
+    afterEach(() => {
+      jest.restoreAllMocks()
+    })
+
+    test('should call service store method and create method of repository ', async () => {
+      jest.spyOn(Client.prototype, 'connect').mockImplementation()
+      jest.spyOn(Client.prototype, 'end').mockImplementation()
+
+      const customerMockedClientPg: QueryResult<any> = {
+        rows: [
+          { name: 'henricker', email: 'henricker@email.com', average_salary: 5000, status: true, id: 1 }
+        ],
+        command: '',
+        rowCount: 1,
+        oid: 0,
+        fields: []
+      }
+
+      jest.spyOn(Client.prototype, 'query').mockImplementation((text, values) => {
+        return customerMockedClientPg
+      })
+
+      jest.spyOn(CustomerRepository.prototype, 'create')
+
+      jest.spyOn(CustomerService.prototype, 'store')
+
+      request.body = { 
+        name: 'henricker', 
+        email: 'henricker@email.com',
+        averageSalary: 500
+      }
+
+      response['send'] = jest.fn()
+
+      await CustomerController.prototype.store(request, response)
+      expect(Client.prototype.query).toBeCalled()
+      expect(CustomerRepository.prototype.create).toBeCalled()
+      expect(CustomerService.prototype.store).toBeCalled()
+    })
+  })
+})
