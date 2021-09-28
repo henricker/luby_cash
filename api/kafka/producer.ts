1c85af7793278901648fe54e879bb7a58eb2bbe9
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-28 18:26:22 -0300

    feat: add producer class

---
+++ api/kafka/producer.ts
@@ -0,0 +1,26 @@
+import { CompressionTypes, Message, Producer as KafkaProducer } from 'kafkajs'
+import kafka from './kafka'
+
+export default class Producer {
+  constructor(private producer: KafkaProducer = kafka.producer({ allowAutoTopicCreation: true })) {}
+
+  public async connect() {
+    await this.producer.connect()
+  }
+
+  public async disconect() {
+    await this.producer.disconnect()
+  }
+
+  public async sendMessage(
+    messages: Array<Message>,
+    topic: string,
+    compression?: CompressionTypes
+  ) {
+    await this.producer.send({
+      topic,
+      messages,
+      compression,
+    })
+  }
+}
