eb001f99dfe9881a111d3bdf44bacd98e960f37d
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-29 09:25:25 -0300

    refactor: change methods to public scope

--- api/kafka/consumer.ts
+++ api/kafka/consumer.ts
@@ -3,29 +3,32 @@ import kafka from './kafka'
 import KafkaConsumerContract from './consumer-service-contract'
 
 export default class Consumer {
+  constructor(
+    private kafkaConsumer: KafkaConsumer = kafka.consumer({
+      allowAutoTopicCreation: true,
+      groupId: 'ms_evaluation',
+    })
+  ) {}
 
-  constructor(private kafkaConsumer: KafkaConsumer = kafka.consumer({ allowAutoTopicCreation: true, groupId: 'ms_evaluation' })) {}
-
-  async connect(): Promise<void> {
+  public async connect(): Promise<void> {
     await this.kafkaConsumer.connect()
   }
 
-  async disconnect(): Promise<void> {
+  public async disconnect(): Promise<void> {
     await this.kafkaConsumer.disconnect()
   }
 
-  async subscribe(topics: ConsumerSubscribeTopic[]): Promise<void> {
-    topics.forEach(async (options) => await this.kafkaConsumer.subscribe({ ...options  }))
+  public async subscribe(topics: ConsumerSubscribeTopic[]): Promise<void> {
+    topics.forEach(async (options) => await this.kafkaConsumer.subscribe({ ...options }))
   }
 
-  async run(contract: KafkaConsumerContract) {
+  public async run(contract: KafkaConsumerContract) {
     await this.kafkaConsumer.run({
       eachMessage: async ({ message, partition, topic }) => {
-        if(topic !== contract.topic)
-          return
-          
+        if (topic !== contract.topic) return
+
         contract.handler({ message, partition, topic })
-      }
+      },
     })
   }
 }
