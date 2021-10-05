e95e11eb6620d8384b4bdb1af5c69b89ec4e1dba
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-04 22:19:38 -0300

    refactor: add new filds to class Customer

--- ms-evaluation/src/application/entity/customer.ts
+++ ms-evaluation/src/application/entity/customer.ts
@@ -1,27 +1,45 @@
+import bcrypt from 'bcrypt'
+import BcryptHelper from '../helpers/bcrypt'
+
 interface ICreateCustomer {
-  name: string
+  full_name: string
   email: string
   average_salary: number
+  current_balance?: number
+  cpf_number: string
   status: boolean
   id?: number,
   created_at?: Date
+  password: string
 }
 
 export default class Customer {
 
   public id?: number
-  public name: string
+  public full_name: string
   public email: string
   public average_salary: number
+  public cpf_number: string
   public status: boolean
+  public phone: string
+  public address: string
+  public city: string
+  public state: string
+  public zipcode: string
+  public current_balance: number
   public created_at?: Date
+  public password: string
   
-  constructor({ name, average_salary, email, status, id, created_at }: ICreateCustomer) {
-    this.name = name
+  constructor({ full_name, average_salary, cpf_number, email, status, id, created_at, current_balance, password }: ICreateCustomer) {
+    this.full_name = full_name
     this.email = email
     this.average_salary = average_salary
+    this.cpf_number = cpf_number
+    this.current_balance = 0
+    current_balance ? this.current_balance = current_balance : ''
     created_at ? this.created_at = created_at : ''
     this.status = status
     id ? this.id = id : ''
+    this.password = password
   }
 }
