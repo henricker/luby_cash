db10a6756486bdd712397b849caad9a990d8fbf2
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-04 22:10:51 -0300

    refactor: add try catch block

--- api/app/Controllers/Http/SessionController.ts
+++ api/app/Controllers/Http/SessionController.ts
@@ -2,12 +2,16 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import SessionValidator from 'App/Validators/SessionValidator'
 
 export default class SessionsController {
-  public async store({ request, auth }: HttpContextContract) {
-    const data = await request.validate(SessionValidator)
-    const token = await auth.use('api').attempt(data.email, data.password, {
-      expiresIn: '1days',
-    })
-
-    return { jwt: token }
+  public async store({ request, auth, response }: HttpContextContract) {
+    try {
+      const data = await request.validate(SessionValidator)
+      const token = await auth.use('api').attempt(data.email, data.password, {
+        expiresIn: '1days',
+      })
+      return { jwt: token }
+    } catch (err) {
+      console.log(err.message)
+      return response.status(400).send({ error: err.message })
+    }
   }
 }
