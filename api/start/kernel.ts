d35b60c91696b80e3bbe948283bd381254a7d048
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-04 22:15:36 -0300

    fix: remove unused middleware authAdmin

--- api/start/kernel.ts
+++ api/start/kernel.ts
@@ -2,5 +2,4 @@ import Server from '@ioc:Adonis/Core/Server'
 Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
 Server.middleware.registerNamed({
   auth: () => import('App/Middleware/Auth'),
-  authAdmin: () => import('App/Middleware/AuthAdmin'),
 })
