2550655bb4a3a822eec7a04a7e661e267c765ec3
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-04 22:12:01 -0300

    refactor: comments all hooks

--- api/app/Models/Pix.ts
+++ api/app/Models/Pix.ts
@@ -1,7 +1,5 @@
 import { DateTime } from 'luxon'
-import { afterCreate, BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
-import User from './User'
-import Producer from '../../kafka/producer'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Pix extends BaseModel {
   @column({ isPrimary: true })
@@ -21,64 +19,65 @@ export default class Pix extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
-
-  @belongsTo(() => User, {
-    foreignKey: 'issuerUser',
-  })
-  public issuerUserModel: BelongsTo<typeof User>
-
-  @belongsTo(() => User, {
-    foreignKey: 'recipientUser',
-  })
-  public recipientUserModel: BelongsTo<typeof User>
-
-  @afterCreate()
-  public static async sendMailerToUsers(pix: Pix) {
-    if (process.env.NODE_ENV === 'testing') return
-
-    const issuerUser = await User.findByOrFail('id', pix.issuerUser)
-    const recipientUser = await User.findByOrFail('id', pix.recipientUser)
-
-    const producer = new Producer()
-
-    await producer.connect()
-
-    await producer.sendMessage(
-      [
-        {
-          value: JSON.stringify({
-            contact: {
-              name: issuerUser.fullName,
-              email: issuerUser.email,
-              transferValue: pix.transferValue,
-              currentBalance: issuerUser.currentBalance,
-              recipientName: recipientUser.fullName,
-            },
-            template: 'issuer-transfer',
-          }),
-        },
-      ],
-      'mailer-event'
-    )
-
-    await producer.sendMessage(
-      [
-        {
-          value: JSON.stringify({
-            contact: {
-              name: recipientUser.fullName,
-              email: recipientUser.email,
-              transferValue: pix.transferValue,
-              currentBalance: recipientUser.currentBalance,
-              issuerName: issuerUser.fullName,
-            },
-            template: 'recipient-transfer',
-          }),
-        },
-      ],
-      'mailer-event'
-    )
-
-    await producer.disconnect()
-  }
 }
+
+//   @belongsTo(() => User, {
+//     foreignKey: 'issuerUser',
+//   })
+//   public issuerUserModel: BelongsTo<typeof User>
+
+//   @belongsTo(() => User, {
+//     foreignKey: 'recipientUser',
+//   })
+//   public recipientUserModel: BelongsTo<typeof User>
+
+//   @afterCreate()
+//   public static async sendMailerToUsers(pix: Pix) {
+//     if (process.env.NODE_ENV === 'testing') return
+
+//     const issuerUser = await User.findByOrFail('id', pix.issuerUser)
+//     const recipientUser = await User.findByOrFail('id', pix.recipientUser)
+
+//     const producer = new Producer()
+
+//     await producer.connect()
+
+//     await producer.sendMessage(
+//       [
+//         {
+//           value: JSON.stringify({
+//             contact: {
+//               name: issuerUser.fullName,
+//               email: issuerUser.email,
+//               transferValue: pix.transferValue,
+//               currentBalance: issuerUser.currentBalance,
+//               recipientName: recipientUser.fullName,
+//             },
+//             template: 'issuer-transfer',
+//           }),
+//         },
+//       ],
+//       'mailer-event'
+//     )
+
+//     await producer.sendMessage(
+//       [
+//         {
+//           value: JSON.stringify({
+//             contact: {
+//               name: recipientUser.fullName,
+//               email: recipientUser.email,
+//               transferValue: pix.transferValue,
+//               currentBalance: recipientUser.currentBalance,
+//               issuerName: issuerUser.fullName,
+//             },
+//             template: 'recipient-transfer',
+//           }),
+//         },
+//       ],
+//       'mailer-event'
+//     )
+
+//     await producer.disconnect()
+//   }
+// }
