e710059373bd3ab648a852e0b39156eff405337c
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-04 22:20:46 -0300

    feat: add update generic method

--- ms-evaluation/src/application/repository/repository.ts
+++ ms-evaluation/src/application/repository/repository.ts
@@ -11,32 +11,38 @@ export abstract class Repository<T> {
   }
 
   public async create (entity: T): Promise<T> {
-    const client = getClient()
-    await client.connect()
-    const values = Object.values(entity)
-    let query = `INSERT INTO ${this.tableName}(`
-    
-    Object.keys(entity).forEach((key) => {
-      query += `${key}, `
-    })
-    query = query.replace(/, $/, '') + ') VALUES('
-
-    Object.keys(entity).forEach((_, index) => {
-      query += `$${index + 1}, `
-    })
-    
-    query = query.replace(/, $/, '') + ') RETURNING *'
-    const result = await client.query(query, values)
-    await client.end()
-
-    const entityObject = new this.classEntity({ ...result.rows[0] })
-    return entityObject
+    try {
+      const client = getClient()
+      await client.connect()
+      const values = Object.values(entity)
+      let query = `INSERT INTO ${this.tableName}(`
+      
+      Object.keys(entity).forEach((key) => {
+        query += `${key}, `
+      })
+      query = query.replace(/, $/, '') + ') VALUES('
+  
+      Object.keys(entity).forEach((_, index) => {
+        query += `$${index + 1}, `
+      })
+      
+      query = query.replace(/, $/, '') + ') RETURNING *'
+      console.log(query)
+      const result = await client.query(query, values)
+      await client.end()
+  
+      const entityObject = new this.classEntity({ ...result.rows[0] })
+      return entityObject
+    } catch(err) {
+      throw new Error(err.message)
+    }
   }
 
   public async findById(id: number): Promise<T | undefined> {
     const client = getClient()
     await client.connect()
     const query = `SELECT * FROM ${this.tableName} WHERE id = $1`
+    console.log(query)
     const values = [ id ]
     const result = await client.query(query, values)
     await client.end()
@@ -45,12 +51,48 @@ export abstract class Repository<T> {
     return entityObject
   }
 
-  public async find({ page = 0, limit = 10 }: findOptions): Promise<T[]> {
+  public async update(fieldsToUpdate: Partial<T>, id: number): Promise<T> {
+    try {
+      const client = getClient()
+      await client.connect()
+      const values = Object.values(fieldsToUpdate).filter((value) => value !== undefined)
+      let query = `UPDATE ${this.tableName} SET `
+      Object.keys(fieldsToUpdate).filter((key) => fieldsToUpdate[key] !== undefined).forEach((key, index) => {
+        query += `${key}=$${index + 1}, `
+      })
+      query = query.replace(/, $/, ` WHERE id=$${values.length + 1} RETURNING *`)
+      console.log(query)      
+      values.push(id)
+      const result = await client.query(query, values)
+      await client.end()
+    
+      const entityObject = result.rows[0] ? new this.classEntity({ ...result.rows[0] }) : undefined
+      return entityObject
+    } catch(err) {
+      throw new Error(err.message)
+    }
+  }
+
+  public async find({ page = 1, limit = 10 }: findOptions, where?: Partial<T> | undefined, dateFilter?: dateFilter): Promise<T[]> {
     const client = getClient()
     await client.connect()
-  //  const query = `SELECT * FROM ${this.tableName} OFFSET ${page * limit} LIMIT ${limit}`
-    const query = `SELECT * FROM ${this.tableName}`
-    const result = await client.query(query)
+    let query = `SELECT * FROM ${this.tableName} `
+
+    if(where) {
+      query += 'WHERE '
+      Object.keys(where).forEach((key, index) => {
+        query+=`${key}=$${index + 1} `
+      })
+    }
+
+    if(dateFilter) {
+      query += `AND created_at >= '${dateFilter.begin_date}' AND created_at <= '${dateFilter.end_date}'`
+    }
+    query += ` OFFSET ${ (page - 1 ) * limit } LIMIT ${limit}`
+
+    console.log(query)
+    const values = where ? Object.values(where) : undefined
+    const result = values ? await client.query(query, values) : await client.query(query) 
     await client.end()
 
     const entities = result.rows.map((row) => new this.classEntity({ ...row }))
@@ -61,4 +103,9 @@ export abstract class Repository<T> {
 interface findOptions {
   page?: number
   limit?: number
+}
+
+export interface dateFilter {
+  begin_date?: string
+  end_date?: string
 }
