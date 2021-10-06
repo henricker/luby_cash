a4bf5a9a76106b1f4b39182da8a8e4a8c5184d32
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-06 14:45:32 -0300

    refactor: remove methods to get by findBy of repository generic

--- ms-evaluation/src/application/repository/customer-repository.ts
+++ ms-evaluation/src/application/repository/customer-repository.ts
@@ -1,41 +1,4 @@
 import { Repository } from "./repository";
-import getClient from '../../infra/database'
 import Customer  from "../entity/customer";
 
-export class CustomerRepository extends Repository<Customer> {
-  public async findByEmail(email: string) {
-    try {
-      const client = getClient()
-      await client.connect()
-      const query = `SELECT * FROM customer WHERE email = $1`
-      console.log(query)
-      const values = [ email ]
-      const result = await client.query(query, values)
-      await client.end()
-    
-      const customer = result.rows[0] ? new Customer({ ...result.rows[0] }) : undefined
-      return customer
-    } catch(err) {
-      console.log(err.message)
-      throw new Error(err.message)
-    }
-  }
-
-  public async findByCPF(cpf: string) {
-    try {
-      const cpf_number = cpf
-      const client = getClient()
-      await client.connect()
-      const query = `SELECT * FROM customer WHERE cpf_number = $1`
-      const values = [ cpf_number ]
-      console.log(query)
-      const result = await client.query(query, values)
-      await client.end()
-    
-      const customer = result.rows[0] ? new Customer({ ...result.rows[0] }) : undefined
-      return customer
-    } catch(err) {
-      throw new Error(err.message)
-    }
-  }
-}
+export class CustomerRepository extends Repository<Customer> {}
