81f1d0aaf42f64dfc3b6dd8e060b8e294decfca0
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-06 07:48:11 -0300

    refactor: set routes

--- ms-evaluation/src/application/api/routes/customer-router.ts
+++ ms-evaluation/src/application/api/routes/customer-router.ts
@@ -5,6 +5,7 @@ const customerRouter = Router()
 const customerController = new CustomerController()
 
 customerRouter.get('/customers', customerController.index)
+customerRouter.get('/customers/email/', customerController.showByEmail)
 customerRouter.get('/customers/:cpf', customerController.show)
 customerRouter.post('/customers', customerController.store)
 customerRouter.put('/customers', customerController.update)
