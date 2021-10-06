a9ea9f35f99e4a4b8b6b9c3867b1c0961be2434e
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-06 07:45:16 -0300

    refactor: set fields to cpfIssuer and cpfRecipient

--- api/app/Models/Pix.ts
+++ api/app/Models/Pix.ts
@@ -1,15 +1,21 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { afterCreate, BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import Customer from './interfaces/customer'
+import axios from 'axios'
+import Env from '@ioc:Adonis/Core/Env'
+import Producer from '../../kafka/producer'
+
+const MS_EVALUATION_ENDPOINT = Env.get('MS_EVALUATION_ENDPOINT')
 
 export default class Pix extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
-  public issuerUser: number
+  public cpfIssuer: string
 
   @column()
-  public recipientUser: number
+  public cpfRecipient: string
 
   @column()
   public transferValue: number
@@ -19,65 +25,56 @@ export default class Pix extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @afterCreate()
+  public static async sendMailerToUsers(pix: Pix) {
+    if (process.env.NODE_ENV === 'testing') return
+
+    const issuerCustomer: Customer = (
+      await axios.get(MS_EVALUATION_ENDPOINT + `/customers/${pix.cpfIssuer}`)
+    ).data['customer']
+
+    const recipientCustomer: Customer = (
+      await axios.get(MS_EVALUATION_ENDPOINT + `/customers/${pix.cpfRecipient}`)
+    ).data['customer']
+
+    const producer = new Producer()
+    await producer.connect()
+    await producer.sendMessage(
+      [
+        {
+          value: JSON.stringify({
+            contact: {
+              name: issuerCustomer.full_name,
+              email: issuerCustomer.email,
+              transferValue: pix.transferValue,
+              currentBalance: issuerCustomer.current_balance,
+              recipientName: recipientCustomer.full_name,
+            },
+            template: 'issuer-transfer',
+          }),
+        },
+      ],
+      'mailer-event'
+    )
+
+    await producer.sendMessage(
+      [
+        {
+          value: JSON.stringify({
+            contact: {
+              name: recipientCustomer.full_name,
+              email: recipientCustomer.email,
+              transferValue: pix.transferValue,
+              currentBalance: recipientCustomer.current_balance,
+              issuerName: issuerCustomer.full_name,
+            },
+            template: 'recipient-transfer',
+          }),
+        },
+      ],
+      'mailer-event'
+    )
+    await producer.disconnect()
+  }
 }
-
-//   @belongsTo(() => User, {
-//     foreignKey: 'issuerUser',
-//   })
-//   public issuerUserModel: BelongsTo<typeof User>
-
-//   @belongsTo(() => User, {
-//     foreignKey: 'recipientUser',
-//   })
-//   public recipientUserModel: BelongsTo<typeof User>
-
-//   @afterCreate()
-//   public static async sendMailerToUsers(pix: Pix) {
-//     if (process.env.NODE_ENV === 'testing') return
-
-//     const issuerUser = await User.findByOrFail('id', pix.issuerUser)
-//     const recipientUser = await User.findByOrFail('id', pix.recipientUser)
-
-//     const producer = new Producer()
-
-//     await producer.connect()
-
-//     await producer.sendMessage(
-//       [
-//         {
-//           value: JSON.stringify({
-//             contact: {
-//               name: issuerUser.fullName,
-//               email: issuerUser.email,
-//               transferValue: pix.transferValue,
-//               currentBalance: issuerUser.currentBalance,
-//               recipientName: recipientUser.fullName,
-//             },
-//             template: 'issuer-transfer',
-//           }),
-//         },
-//       ],
-//       'mailer-event'
-//     )
-
-//     await producer.sendMessage(
-//       [
-//         {
-//           value: JSON.stringify({
-//             contact: {
-//               name: recipientUser.fullName,
-//               email: recipientUser.email,
-//               transferValue: pix.transferValue,
-//               currentBalance: recipientUser.currentBalance,
-//               issuerName: issuerUser.fullName,
-//             },
-//             template: 'recipient-transfer',
-//           }),
-//         },
-//       ],
-//       'mailer-event'
-//     )
-
-//     await producer.disconnect()
-//   }
-// }
