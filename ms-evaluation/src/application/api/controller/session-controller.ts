b427369530c48dbb9e38a07964189b060229a505
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-06 07:47:56 -0300

    style: add all comments to unused controller

--- ms-evaluation/src/application/api/controller/session-controller.ts
+++ ms-evaluation/src/application/api/controller/session-controller.ts
@@ -1,29 +1,29 @@
-import { Request, Response } from 'express'
-import BcryptHelper from '../../helpers/bcrypt'
-import JwtHelper from '../../helpers/jwt'
-import { CustomerRepository } from '../../repository/customer-repository'
+// import { Request, Response } from 'express'
+// import BcryptHelper from '../../helpers/bcrypt'
+// import JwtHelper from '../../helpers/jwt'
+// import { CustomerRepository } from '../../repository/customer-repository'
 
-const customerRepository = new CustomerRepository()
+// const customerRepository = new CustomerRepository()
 
-export default class SessionController {
+// export default class SessionController {
 
-  public async store(request: Request, response: Response) {
-    try {
-      const { email, password } = request.body
-      const customer = await customerRepository.findByEmail(email)
+//   public async store(request: Request, response: Response) {
+//     try {
+//       const { email, password } = request.body
+//       const customer = await customerRepository.findByEmail(email)
 
-      if(!customer)
-        throw new Error('invalid credentials')
+//       if(!customer)
+//         throw new Error('invalid credentials')
 
-      const checkPassword = await BcryptHelper.compare(password, customer.password)
+//       const checkPassword = await BcryptHelper.compare(password, customer.password)
 
-      if(!checkPassword)
-        throw new Error('invalid credentials')
+//       if(!checkPassword)
+//         throw new Error('invalid credentials')
 
-      const token = JwtHelper.generateToken({ id: customer.id })
-      return response.status(200).send({ token })
-    } catch(err) {
-      return response.status(400).send({ errors: [{ message: err.message }] })
-    }
-  }
-}
+//       const token = JwtHelper.generateToken({ id: customer.id })
+//       return response.status(200).send({ token })
+//     } catch(err) {
+//       return response.status(400).send({ errors: [{ message: err.message }] })
+//     }
+//   }
+// }
