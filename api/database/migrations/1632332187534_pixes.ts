b310150692a41566dd814399607b28c67923cc27
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-06 07:46:29 -0300

    refactor: change fields to cpfIssuer and cpfRecipient

--- api/database/migrations/1632332187534_pixes.ts
+++ api/database/migrations/1632332187534_pixes.ts
@@ -6,12 +6,8 @@ export default class Pixes extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table
-        .integer('cpf_issuer')
-        .unsigned()
-      table
-        .integer('cpf_recipient')
-        .unsigned()
+      table.string('cpf_issuer').unsigned()
+      table.string('cpf_recipient').unsigned()
       table.double('transfer_value').notNullable().unsigned()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
