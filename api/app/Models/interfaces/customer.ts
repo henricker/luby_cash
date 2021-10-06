2e5418904464508c78a45af8c6974b41cdb50046
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-06 07:45:35 -0300

    feat: create customer interface

---
+++ api/app/Models/interfaces/customer.ts
@@ -0,0 +1,10 @@
+export default interface Customer {
+  full_name: string
+  email: string
+  average_salary: number
+  cpf_number: string
+  current_balance: number
+  created_at: Date
+  status: boolean
+  id: number
+}
