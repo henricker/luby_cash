784272137f7c717cdb1332e53f3a75c85bb6eebe
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-06 07:53:33 -0300

    chore: remove jsonwebtoken and sessionController

--- ms-evaluation/src/application/api/routes/index.ts
+++ ms-evaluation/src/application/api/routes/index.ts
@@ -1,7 +1,7 @@
 import Router from 'express'
 import customerRouter from './customer-router'
-// import sessionRouter from './session-router'
+
 const routes = Router()
 routes.use(customerRouter)
-// routes.use(sessionRouter)
+
 export default routes
--- ms-evaluation/src/application/helpers/jwt.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-import env from '../../../env'
-
-export default class JwtHelper {
-  public static generateToken(payload: {}) {
-    const token = jwt.sign(payload, env.jwt.secret)
-    return token
-  }
-}
